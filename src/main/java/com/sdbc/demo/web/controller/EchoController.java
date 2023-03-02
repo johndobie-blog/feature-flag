0ac85ad2f2621afb20f351e54923097aae6cf35b
John Dobie <john@dobie.co.uk>
2023-03-02 08:34:26 +0000

    tidy code

--- src/main/java/com/sdbc/demo/web/controller/EchoController.java
+++ src/main/java/com/sdbc/demo/web/controller/EchoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@ConditionalOnProperty(name = "echo.enabled", matchIfMissing = false)
+@ConditionalOnProperty(name = "echo.enabled", havingValue = "true", matchIfMissing = false)
 public class EchoController {
 
     public static final String ECHO_ENDPOINT = "/echo";
